c9d04b99d13a4b9405529d2d03d5481087c535c4
PRAVEEN KALAVAKUNTLA <99800759+PRAVEENKALAVAKUNTLA@users.noreply.github.com>
2022-02-24 12:29:36 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package Fresh;
+
+public class Employee{
+    String firstname;
+    String lastname;
+    int age;
+    int salary;
+
+    public Employee(String firstname, String lastname, int age, int salary){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.salary = salary;
+    }
+}
